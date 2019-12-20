73747827c590a9eb84b3b3204e99c043b7e55a32
karapv <andrewkarapenkov@gmail.com>
2019-12-21 00:50:23 +0200

    Add Main Page.Version 1.0.0

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,100 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-   //  // Слайдер видео
-   //  $(".video-slider").owlCarousel({
-   //      loop: true,
-   //      navText : ["",""],
-   //      autoplayHoverPause: true,
-   //      lazyLoad: true,
-   //      autoplay: true,
-   //      nav: false,
-   //      dots: true,
-   //      responsive: {
-   //          0:{
-   //              items: 1,
-   //              margin: 0,
-   //              autoWidth: false,
-   //              nav: false,
-   //              dots: true
-   //          },
-   //          680:{
-   //              items: 2,
-   //              margin: 20,
-   //              autoWidth: false,
-   //              nav: false,
-   //              dots: true
-   //          },
-   //          993: {
-   //              items: 3,
-   //              margin: 40,
-   //              autoWidth: true,
-   //              center: false,
-   //              nav: false,
-   //              dots: true
-   //          }
-   //      }
-   //  });
-   //  $('.youtube').click(function () {
-   //      $('.video-slider').trigger('stop.owl.autoplay');
-   //  });
-   //  // Слайдер отзывов
-   //  $(".rewiews-slider").owlCarousel({
-   //      items: 3,
-   //      nav: false,
-   //      dots: true,
-   //      loop: true,
-   //      autoplayHoverPause: true,
-   //      lazyLoad: true,
-   //      autoplay: true,
-   //      responsive: {
-   //          0: {
-   //              items: 1,
-   //              margin: 0,
-   //              autoWidth: false,
-   //              center: true
-   //          },
-   //          993: {
-   //              items: 3,
-   //              margin: 30,
-   //              autoWidth: true,
-   //              center: true
-   //          }
-   //      }
-   //  });
-   //  // Попапы
-   //  $(document).on('click','.btn-tarif',function (e) {
-   //      e.preventDefault();
-   //      let link = $(this).attr('href');
-   //      $('.popup-background').fadeIn('slow');
-   //      $(link).fadeIn('slow');
-   //  });
-   //  $('.popup-close').click(function () {
-   //      $(this).parent().parent().fadeOut('slow');
-   //      $('.popup-background').fadeOut('slow');
-   //  });
-   //  // Активация анимации
-   //  new WOW().init();
-   // // Мобильное меню
-   // $('.toggle-menu').click(function () {
-   //     if($(this).hasClass('active')){
-   //         $(this).addClass('disabled');
-   //         $(this).removeClass('active');
-   //         $('.header-nav').slideUp('slow');
-   //     }else{
-   //         $(this).addClass('active');
-   //         $(this).removeClass('disabled');
-   //         $('.header-nav').slideDown('slow');
-   //     }
-   // });
     let lazyLoadInstance = new LazyLoad({
         elements_selector: ".lazy"
     });
     lazyLoadInstance.update();
-   // Плавный скролл
-    $("a.scroll-to").on("click", function(e){
-        e.preventDefault();
-        var anchor = $(this).attr('href');
-        $('html, body').stop().animate({
-            scrollTop: $(anchor).offset().top - 60
-        }, 3500);
+    $('.toggle-cat').mouseenter(function(){
+        $('.bottom-header-cat-menu').css({'display':'block'});
+        });
+    $('.bottom-header-cat-menu').mouseleave(function() {
+        $('.bottom-header-cat-menu').css({'display': 'none'});
     });
 });
