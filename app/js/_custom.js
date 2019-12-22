157fa938604a2a6e684e46e4f3eb483d1bbb2427
karapv <andrewkarapenkov@gmail.com>
2019-12-22 16:27:44 +0200

    catalog is ready

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -20,6 +20,14 @@ document.addEventListener("DOMContentLoaded", function() {
     $('.mobile-toggle-cat').click(function () {
         $('.mobile-header-nav').fadeToggle('slow');
     });
+    $('.catalog-filter-button').click(function () {
+       $(this).toggleClass('active');
+       $(this).parent().next('.catalog-filter-list').fadeToggle('slow');
+    });
+    $('.sort-date-btn').click(function () {
+       $(this).toggleClass('active');
+       $(this).next('.sort-date-list').fadeToggle('slow');
+    });
     $(".mobile-main-our-categories-list").owlCarousel({
         loop: true,
         autoplayHoverPause: true,
@@ -60,4 +68,45 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     });
+   $('.catalog-tab-item').click(function (e) {
+       e.preventDefault();
+       if($(this).hasClass('disabled')){
+           return
+       }
+       const activeTab = $('.catalog-tab-item.active');
+       let currentTab = e.target.href,
+           currentTabText = $(this).text(),
+           oldTabText = activeTab.text(),
+           oldTab = activeTab.attr('href');
+       $(this).find('a').text(oldTabText).attr('href',oldTab);
+       activeTab.find('a').text(currentTabText).attr('href',currentTab);
+       currentTab = currentTab.replace(/.*\/#/g,'');
+       $('#'+currentTab).addClass('active').siblings('.tab').removeClass('active');
+   });
+   $('.catalog-tab-item-button').click(function () {
+      $(this).toggleClass('active');
+      $('.catalog-tab-item:not(.active)').fadeToggle('slow');
+   });
+   $('.mobile-catalog-filter-button').click(function () {
+      $(this).toggleClass('active');
+      $('.catalog-filter-item:not(.mobile-catalog-filter-item)').fadeToggle('slow');
+   });
+    $('.docs-list').owlCarousel({
+        autoplayHoverPause: true,
+        autoplay: false,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        responsive: {
+            0:{
+                items: 3
+            },
+            800:{
+                items: 3
+            },
+            900: {
+                items: 9
+            }
+        }
+    })
 });
